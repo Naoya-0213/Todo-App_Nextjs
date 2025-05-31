25ee0441bfac668ac7108fb6274840e0afb30d95
Naoya Noguchi <naoya.work0213@gmail.com>
2025-05-31 11:50:27 +0900

    signin実装

--- src/app/components/signin/page.tsx
+++ src/app/components/signin/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { login, signup } from './action'
 
-const signin = () => {
+export default function LoginPage() {
   return (
-    <div>signin</div>
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+    </form>
   )
 }
-
-export default signin
-
-
