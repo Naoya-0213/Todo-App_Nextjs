46ddb68fb03d8e0062131a7cd7924ca43a835382
Naoya Noguchi <naoya.work0213@gmail.com>
2025-06-01 13:30:51 +0900

    supabase-lisner完成！

--- src/app/components/supabase-lisner.tsx
+++ src/app/components/supabase-lisner.tsx
@@ -2,10 +2,42 @@
 
 "use server";
 
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
 import { Database } from "../../lib/database.types";
 import { Navigation } from "./navigation";
+import { createClient } from "../utils/supabase/supabase-server";
 
 // 認証状態の監視
-export const SupabaseLisner = async () => {};
+export const SupabaseLisner = async () => {
+  const supabase = await createClient<Database>();
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // プロフィールの取得
+  let profile = null;
+  if (session) {
+    const { data: currentProfile } = await supabase
+      .from("profiles") // テーブル指定
+      .select("*") // すべてのカラムを取得
+      .eq("id", session.user.id) // 条件：idが一致
+      .single(); // 結果は1件だけ期待
+
+    profile = currentProfile;
+
+    // メールアドレスを変更した場合、プロフィールを更新
+    if (currentProfile && currentProfile.email !== session.user.email) {
+      const { data: updatedProfile } = await supabase
+        .from("profiles")
+        .update({ email: session.user.email }) // 新しいメアドに更新
+        .match({ id: session.user.id }) // このidが対象
+        .select("*")
+        .single();
+
+      profile = updatedProfile;
+    }
+  }
+
+  return <Navigation session={session} profile={profile} />;
+};
--- src/app/utils/supabase/supabase-server.ts
+++ src/app/utils/supabase/supabase-server.ts
@@ -19,9 +19,8 @@ export async function createClient<Schema>() {
               cookieStore.set(name, value, options)
             );
           } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // `setAll` メソッドは Server Component から呼び出されました。
+            // ミドルウェアでユーザーセッションを更新している場合は、この警告は無視してかまいません。
           }
         },
       },
